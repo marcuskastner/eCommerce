dbe180f8dac9420d31e45930110e51267db5d9d0
Marcus Kastner <marcushkastner@gmail.com>
2022-08-14 15:53:27 -0700

    try 2

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,7 +3,7 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import tw from "twin.macro"
 import Hero from "../components/molecule/Hero"
 import { useStateContext } from "../context/StateContext"
-import HeroImage from "../images/hero/hero_background.png"
+import HeroImage from "../images/hero/hero_Background.png"
 
 const IndexPage = () => {
   const { setShowCart } = useStateContext()
